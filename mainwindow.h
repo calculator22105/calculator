4b53ea591ab815b99ea3744b1b6cbb2f24530e26
DenisKnaub <66167786+DenisKnaub@users.noreply.github.com>
2020-06-23 13:20:43 +0300

    Create mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+private slots:    //
+    void digit_pressed();
+    void on_pushButton_decimal_clicked();
+    void unary_operation_pressed();
+
+    void on_pushButton_clear_clicked();
+    void on_pushButton_equals_clicked();
+    void binary_operation_pressed();
+
+
+};
+#endif // MAINWINDOW_H
