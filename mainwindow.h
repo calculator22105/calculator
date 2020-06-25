6ac7b28606d2f781219aa294ee12287875621d82
Ефимова Дарья <66119268+mu-stardash@users.noreply.github.com>
2020-06-25 15:21:51 +0300

    Update mainwindow.h
    Изменение заголовочного файла главного окна, так как добавили вторую форму

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "form.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,7 +18,10 @@ public:
 
 private:
     Ui::MainWindow *ui;
-private slots:    //
+    
+    Form *sWindow;                        // второе окно
+    
+private slots:
     void digit_pressed();                 // при нажатии цифры
     void on_pushButton_decimal_clicked(); // при нажатии "." (десятичной дроби)
     void unary_operation_pressed();       // для унарных операций
@@ -26,6 +30,8 @@ private slots:    //
     void binary_operation_pressed();      // для бинарных операций
     void on_pushButton_back_clicked();    // для удаления предыдущего символа
     void on_pushButton_deg_clicked();     // для перевода значений из радиан в градусы
+    
+    void on_radioButton_clicked();        // для переключения на другое окно
 };
 
 #endif // MAINWINDOW_H
